9c60b744b213d2b859d8ee62d2fad95f8558d798
zsxm1998 <zsxm1998@qq.com>
2022-03-15 16:53:54 +0000

    记录霍夫曼误差

--- zstage/run.py
+++ zstage/run.py
@@ -19,29 +19,33 @@ import json
 # for byte_rate in ['64', '128', '256']:
 #     reconstruct(byte_rate)
 
-# class L2ReconstructionLoss(nn.Module):
-#     def __init__(self):
-#         super(L2ReconstructionLoss, self).__init__()
+class L2ReconstructionLoss(nn.Module):
+    def __init__(self):
+        super(L2ReconstructionLoss, self).__init__()
 
-#     def forward(self, reconstructed, origin):
-#         assert reconstructed.shape == origin.shape, f'reconstructed.shape({reconstructed.shape}) should be equal to origin.shape({origin.shape})'
-#         return torch.linalg.norm(reconstructed-origin, dim=1, ord=2).mean()
+    def forward(self, reconstructed, origin):
+        assert reconstructed.shape == origin.shape, f'reconstructed.shape({reconstructed.shape}) should be equal to origin.shape({origin.shape})'
+        return torch.linalg.norm(reconstructed-origin, dim=1, ord=2).mean()
 
-# file_list = sorted(os.listdir('query_feature'))
-# feature_list = []
-# for file in file_list:
-#     feature_list.append(np.fromfile(os.path.join('query_feature', file), dtype='<f4')[:128])
-# features = torch.from_numpy(np.stack(feature_list, axis=0))
-# del feature_list
-# for byte_rate in ['64', '128', '256']:
-#     reconstruct_feature_list = []
-#     for file in file_list:
-#         reconstruct_feature_list.append(np.fromfile(os.path.join('reconstructed_query_feature', byte_rate, file), dtype='<f4')[:128])
-#     reconstruct_features = torch.from_numpy(np.stack(reconstruct_feature_list, axis=0))
-#     del reconstruct_feature_list
-#     print(f'byte_rate {byte_rate} L2 loss:', L2ReconstructionLoss()(reconstruct_features, features))
+file_list = sorted(os.listdir('query_feature'))
+feature_list = []
+for file in file_list:
+    feature_list.append(np.fromfile(os.path.join('query_feature', file), dtype='<f4')[:128])
+features = torch.from_numpy(np.stack(feature_list, axis=0))
+del feature_list
+for byte_rate in ['64', '128', '256']:
+    reconstruct_feature_list = []
+    for file in file_list:
+        reconstruct_feature_list.append(np.fromfile(os.path.join('/nfs3-p1/zsxm/naic/huffman/reconstructed_features', byte_rate, file), dtype='<f4')[:128])
+    reconstruct_features = torch.from_numpy(np.stack(reconstruct_feature_list, axis=0))
+    del reconstruct_feature_list
+    print(f'byte_rate {byte_rate} L2 loss:', L2ReconstructionLoss()(reconstruct_features, features))
 # byte_rate 64 L2 loss: tensor(0.1034)
 # byte_rate 128 L2 loss: tensor(0.1034)
+# byte_rate 256 L2 loss: tensor(5.8545e-05)
+# huffman误差
+# byte_rate 64 L2 loss: tensor(0.6154)
+# byte_rate 128 L2 loss: tensor(0.4219)
 # byte_rate 256 L2 loss: tensor(5.8545e-05)
 
 # def calc_acc_reid(query, gallary, labels):
@@ -117,37 +121,37 @@ import json
 # for byte_rate in ['64', '128', '256']:
 #     reid(byte_rate)
 
-with open('/nfs3-p2/zsxm/naic/rematch/train/val_list.txt', 'r') as f:
-    lines = f.readlines()
-file_to_label = {}
-for line in lines:
-    fname, label = line.split()[:2]
-    fname = fname.split('.')[0]+'.png'
-    label = int(label)
-    file_to_label[fname] = label
-for byte_rate in ['64', '128', '256']:
-    with open(f'reid_results/{byte_rate}.json', 'r') as jf:
-        res = json.load(jf)
-    acc1, mAP = 0, 0
-    for k, v in res.items():
-        ap = 0
-        label = file_to_label[k]
-        rank_label = np.array([file_to_label[fn] for fn in v if fn != k])
-        if rank_label[0] == label:
-            acc1 += 1 
-        correct_rank_idx = np.argwhere(rank_label==label).flatten()
-        correct_rank_idx = correct_rank_idx[correct_rank_idx<100]
-        n_correct = len(correct_rank_idx)
-        if n_correct > 0:
-            d_recall = 1 / n_correct
-            for j in range(n_correct):
-                precision = (j+1) / (correct_rank_idx[j]+1)
-                ap += d_recall * precision
-        mAP += ap
-    acc1 /= len(res)
-    mAP /= len(res)
-    ACC_reid = (acc1 + mAP) / 2
-    print(f'{byte_rate}', ACC_reid, acc1, mAP)
+# with open('/nfs3-p2/zsxm/naic/rematch/train/val_list.txt', 'r') as f:
+#     lines = f.readlines()
+# file_to_label = {}
+# for line in lines:
+#     fname, label = line.split()[:2]
+#     fname = fname.split('.')[0]+'.png'
+#     label = int(label)
+#     file_to_label[fname] = label
+# for byte_rate in ['64', '128', '256']:
+#     with open(f'reid_results/{byte_rate}.json', 'r') as jf:
+#         res = json.load(jf)
+#     acc1, mAP = 0, 0
+#     for k, v in res.items():
+#         ap = 0
+#         label = file_to_label[k]
+#         rank_label = np.array([file_to_label[fn] for fn in v if fn != k])
+#         if rank_label[0] == label:
+#             acc1 += 1 
+#         correct_rank_idx = np.argwhere(rank_label==label).flatten()
+#         correct_rank_idx = correct_rank_idx[correct_rank_idx<100]
+#         n_correct = len(correct_rank_idx)
+#         if n_correct > 0:
+#             d_recall = 1 / n_correct
+#             for j in range(n_correct):
+#                 precision = (j+1) / (correct_rank_idx[j]+1)
+#                 ap += d_recall * precision
+#         mAP += ap
+#     acc1 /= len(res)
+#     mAP /= len(res)
+#     ACC_reid = (acc1 + mAP) / 2
+#     print(f'{byte_rate}', ACC_reid, acc1, mAP)
 # 64 0.8679601164035629 0.905207780298277 0.8307124525088488
 # 128 0.8679335389783798 0.9051595154206284 0.830707562536131
 # 256 0.9239048069654079 0.9532313335585695 0.8945782803722463
